c4f6e284a5e1c6cf2e6296999c167f1303f75b06
akashkuchankar <kuchankarakash@gmail.com>
2022-10-20 21:22:25 -0700

    Add project files.

---
+++ arrary class/Program.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace arrary_class
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {                                                           // LIFO (list in first out)
+//            Stack s = new Stack();
+//            s.Push(10);                                            // add to stack
+//            s.Push(20);
+//            s.Push(30);
+
+//            s.Pop();                                              // remove the last element from the collections
+
+//            foreach (var item in s)
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//            Console.WriteLine("last element " + s.Peek());        // display the last element
+
+
+//        }
+//    }
+//}
